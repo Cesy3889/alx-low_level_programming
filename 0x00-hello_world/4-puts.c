0ad1e54ecbc93cc5596d2cb40c74f22f115b8c50
celine sitina <sitinaceline@gmail.com>
2022-03-10 23:46:30 +0300

    added 4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <studio.h>
+/**
+*Declaration: main - prints out a string to stdout
+*Return : 0 if success
+*/
+Int main ( void)
+{
+puts ("\" programming is like building a multilingual puzzle");
+return 0 ;
+}
